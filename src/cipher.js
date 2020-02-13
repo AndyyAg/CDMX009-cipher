d445b3bbd37cd457eb774b4279d540b9ab5ed4e4
AndyyAg <andy.ag2303@gmail.com>
2020-02-13 13:35:51 -0600

    Modifiqué html/js

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,61 @@
 const cipher = {
-//    propiedades
+    texto: document.getElementById("mensajeACodificar").value, 
+    desplazamiento: parseInt(document.getElementById("cantidadDesplazamiento").value),
 
- //   métodos(funcion)
+    //FUNCIÓN PARA CIFRAR STRING//
+    cifrar: function (texto, desplazamiento){
+        let resultCifer = "";  //Almacenará el resultado final//
 
+        for(var i=0; i < texto.length; i++){
+            let ascii = texto.charCodeAt(i);
+
+            let formulaCifrar;  //Almacena fórmul del resultado
+            let nuevaLetra;   // Almacena nuevo valor de la letra decifrada
+
+            if (65 >= ascii && ascii <= 90) { //Revisar en mayúsculas//
+                formulaCifrar = (ascii - 65 + desplazamiento) % 26 + 65;
+                nuevaLetra = String.fromCharCode(formulaCifrar);
+                resultCifer+=nuevaLetra;
+            } else if (97 <= ascii && ascii <= 122){
+
+                formulaCifrar = (ascii -97 - desplazamiento) %26 + 97;
+                nuevaLetra = String.fromCharCode(formulaCifrar);
+                resultCifer+=nuevaLetra;
+            }else if (ascii === 32){
+                resultCifer += " ";
+            }else{
+                break;
+            }
+        }
+        return resultCifer;
+    }
+    
+    //FUNCIÓN PARA DESCIFRAR STRING//
+    ,descifrar: function (texto, desplazamiento){
+        let resultDecifer ="";  //Almacenará valor del string descifrado//
+        
+        for(var i=0; i < texto.length; i++){
+            let ascii = texto.charCodeAt(i);
+
+            let formulaDescifrar;
+            let nuevaLetra;
+
+            if (65 >= ascii && ascii <= 90){
+                formulaDescifrar = (ascii - 13 - desplazamiento) %26 + 65;
+                nuevaLetra = String.fromCharCode(formulaDescifrar);
+                resultDecifer+=nuevaLetra;
+            } else if (ascii >= 97 && ascii <= 122){
+                formulaDescifrar = (ascii -45 - desplazamiento) %26 + 97;
+                nuevaLetra = String.fromCharCode(formulaDescifrar);
+                resultDecifer+=nuevaLetra;
+            } else if (ascii === 32){
+                resultDecifer += " ";
+            } else{
+                break;
+            }
+        }
+        return resultDecifer;
+    },
 };
 
-
 export default cipher;
-
--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import cipher from './cipher.js';
 
+
+const string ='';
+const number = 0;
+
+let ButtonDecode = document.getElementById("decode");
+let ButtonEncode = document.getElementById("encode");
+
+function imprimirDatos(){
+    string = document.getElementById("mensajeACodificar").value;
+    number = document.getElementById("cantidadDesplazamiento").value;
+
+} 
+
+ButtonEncode.onclick = function(){
+    actionData();
+    document.getElementById("result").value = cipher.cifrar(string,number);
+    document.getElementById("result").innerHTML = cipher.cifrar(string, number);
+};
+
+ButtonDecode.onclick = function () {
+    actionData();
+    document.getElementById("result").value = cipher.descifrar(string, number);
+    document.getElementById("result").innerHTML = cipher.descifrar(string, number);
+
+};
+
+
 console.log(cipher);
 
